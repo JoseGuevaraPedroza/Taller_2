305d1f57d783337820c84f85dc5619be0d20c55d
JoseGuevaraPedroza <111126930+JoseGuevaraPedroza@users.noreply.github.com>
2023-02-19 19:59:30 -0500

    bb

--- src/model/Restaurante.java
+++ src/model/Restaurante.java
@@ -9,32 +9,54 @@ public class Restaurante {
 	private ArrayList<Ingrediente> ingredientes;
 	private ArrayList<Producto> menuBase;
 	private HashMap<String, Pedido>  pedidos = new HashMap<String, Pedido>();
- public Restaurante() {
+
+
+public Restaurante() throws IOException 
+{
 	// TODO Auto-generated constructor stub
+	
 }
-public ArrayList<Producto> getMenuBase() throws IOException
+
+public ArrayList<Producto> getMenuBase()
+{
+	
+}
+public ArrayList<Ingrediente> getIngredientes()
+{
+}
+
+public void CargarInformacionRestaurante(File archivoIngredientes, File archivoMenu, File archivoCombos) 
+{
+
+}
+
+private void cargarIngredientes(File archivoIngredientes) throws IOException 
+{
+	ArrayList<Ingrediente> result = new ArrayList<Ingrediente>();
+	String[] texto_archivo=File_loader.Cargar_archivo(archivoIngredientes);
+	for (int i = 0; i < texto_archivo.length; i=+2) 
+	{
+		String name = texto_archivo[i];
+		int price = Integer.parseInt(texto_archivo[i+1]);
+		Ingrediente p = new Ingrediente(name, price);
+		result.add(p);
+	}
+	this.ingredientes=result;
+}
+
+private void cargarMenu(File archivoMenu) throws IOException 
 {
 	ArrayList<Producto> result = new ArrayList<Producto>();
-	File path = new File("data/menu.txt");
-	String[] texto_archivo=File_loader.Cargar_archivo(path);
+	String[] texto_archivo=File_loader.Cargar_archivo(archivoMenu);
 	for (int i = 0; i < texto_archivo.length; i=+2) 
 	{
 		String name = texto_archivo[i];
 		int price = Integer.parseInt(texto_archivo[i+1]);
 		Producto p = new ProductoMenu(name, price);
+		result.add(p);
 	}
-	return result;
+	this.menuBase=result;
 }
-public ArrayList<Ingrediente> getIngredientes()
-{}
-public void CargarInformacionRestaurante(File archivoIngredientes, File archivoMenu, File archivoCombos) 
-{}
-
-private void cargarIngredientes(File archivoIngredientes) 
-{}
-
-private void cargarMenu(File archivoMenu) 
-{}
 
 private void cargarCombos(File archivoCombos) 
 {}
